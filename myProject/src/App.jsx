a8c4e2e9610201eed612c22c6aca404d22942f91
Jyoti Singh <pur583395@gmail.com>
2026-01-12 15:54:09 +0530

    url update

--- myProject/src/App.jsx
+++ myProject/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import MapView from "./components/ViewMap.jsx";
 import SpotForm from "./components/SpotForm.jsx";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "http://localhost:5000/api/spots";
 
 const App = () => {
   const [selectedCoords, setSelectedCoords] = useState(null);
